5e77326cdffd166db981312b81d920f76cbe9b3c
cw-matiastorres <matiastorres@coowise.com>
2024-11-30 04:27:27 -0300

    Add web scraping functionality to Program.cs in OllamaScraperApp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static async Task Main(string[] args)
+    {
+        string url = "https://ollama.com/library/llama3.2";
+        using HttpClient client = new HttpClient();
+        var response = await client.GetStringAsync(url);
+
+        Console.WriteLine(response); // Display raw HTML for debugging
+        // Here, you can use regex or an HTML parser to extract meaningful content
+    }
+}
